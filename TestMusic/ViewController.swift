df1bdca597e87f41e3350007b052e1abc0157bc3
McKayne <mc.kayne@yahoo.com>
2018-10-05 14:30:55 +0300

    Initial Commit

---
+++ TestMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMusic
+//
+//  Created by для интернета on 05.10.18.
+//  Copyright © 2018 для интернета. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
